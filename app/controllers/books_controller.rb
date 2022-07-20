596a45e1dcdc27bef4ac5f643a500e654df426da
EC2 Default User <ec2-user@ip-172-31-9-204.ec2.internal>
2022-07-20 12:33:30 +0000

    [books.show/index~users.show/index画面作成]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,44 @@
 class BooksController < ApplicationController
-  def new
+  def new 
+    @book = Book.new
+    @user = User.new
   end
   
   def index
+    @users = User.all
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @users = User.all
+    @user = current_user
+    @books = Book.all
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @books = Book.all
+    @book = Book.find(params[:id])
   end
 
   def create
+     @book = Book.new(book_params)
+     @book.user_id = current_user.id
+     @book.save
+     redirect_to book_path(current_user.id)
   end
-  
+   
   def destroy
+    @book = Book.find(params[:id]) 
+    @book.destroy
+    redirect_to books_path #indexは(id)をつける必要ない。本棚に立って本を見ている状態
   end
 
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
   def index
-    @user = User.all
+    @users = User.all
+    @user = current_user
+    @books = Book.all
   end
   
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
   
   def edit
@@ -12,6 +19,9 @@ class UsersController < ApplicationController
   end
 
   def update
+     user = User.find(params[:id])
+     user.update(user_params)
+     redirect_to users_path(user.id)
   end
   
   private
